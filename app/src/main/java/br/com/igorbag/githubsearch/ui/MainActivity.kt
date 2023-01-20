f56f0ba8389a6e0817a2565204760fc47e846761
Diego Rocha <diegorsf.dns@gmail.com>
2023-01-19 23:22:48 -0300

    Feature: Added GitHub Search
    - Improved UI Design
    - Added a circular progress bar

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -6,11 +6,20 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.data.GitHubService
 import br.com.igorbag.githubsearch.domain.Repository
+import br.com.igorbag.githubsearch.ui.adapter.RepositoryAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnConfirm: Button
     lateinit var repoList: RecyclerView
     lateinit var githubApi: GitHubService
+    lateinit var progressBar: ProgressBar
+    lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,28 +37,31 @@ class MainActivity : AppCompatActivity() {
         setupListeners()
         showUserName()
         setupRetrofit()
-        getAllReposByUserName()
     }
 
     private fun setupView() {
         userName = findViewById(R.id.et_user_name)
         btnConfirm = findViewById(R.id.btn_confirm)
         repoList = findViewById(R.id.rv_repo_list)
+        progressBar = findViewById(R.id.progress_circular)
+        resultText = findViewById(R.id.tv_result_info)
     }
 
-    //metodo responsavel por configurar os listeners click da tela
     private fun setupListeners() {
         btnConfirm.setOnClickListener {
-            // Action
             val currentUser = userName.text.trim().toString()
-            if (currentUser.isNotEmpty()){
+
+            if (currentUser.isNotBlank()) {
+                progressBar.isVisible = true
+
                 saveUserLocal(currentUser)
+                getAllReposByUserName(currentUser)
             }
         }
     }
 
     private fun saveUserLocal(currentUser: String) {
-        val editor =  getSharedPreferences("GITHUB_USER", Context.MODE_PRIVATE).edit()
+        val editor = getSharedPreferences("GITHUB_USER", Context.MODE_PRIVATE).edit()
         editor.apply {
             putString("current_user", currentUser)
             apply()
@@ -61,38 +75,57 @@ class MainActivity : AppCompatActivity() {
 
     private fun showUserName() {
         val currentUser = getUserLocal()
-        if (!currentUser.isNullOrEmpty()){
+
+        if (!currentUser.isNullOrEmpty())
             userName.setText(currentUser)
+    }
+
+    private fun setupRetrofit() {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        githubApi = retrofit.create(GitHubService::class.java)
+    }
+
+    private fun getAllReposByUserName(user: String) {
+
+        githubApi.getAllRepositoriesByUser(user).enqueue(object : Callback<List<Repository>> {
+            override fun onResponse(
+                call: Call<List<Repository>>,
+                response: Response<List<Repository>>
+            ) {
+                if (response.isSuccessful) {
+                    setupAdapter(response.body()!!)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
+                resultText.isVisible = false
+            }
+        })
+    }
+
+    private fun setupAdapter(list: List<Repository>) {
+
+        val repoAdapter = RepositoryAdapter(list)
+        repoList.adapter = repoAdapter
+
+        progressBar.isVisible = false
+        if (!resultText.isVisible) resultText.isVisible = true
+
+        repoAdapter.repoItemListener = { repoCard ->
+            openBrowser(repoCard.htmlUrl)
+        }
+
+        repoAdapter.btnShareListener = { repoUrl ->
+            shareRepositoryLink(repoUrl.htmlUrl)
         }
     }
 
-    //Metodo responsavel por fazer a configuracao base do Retrofit
-    fun setupRetrofit() {
-        /*
-       @TODO 5 -  realizar a Configuracao base do retrofit
-       Documentacao oficial do retrofit - https://square.github.io/retrofit/
-       URL_BASE da API do  GitHub= https://api.github.com/
-       lembre-se de utilizar o GsonConverterFactory mostrado no curso
-    */
-    }
-
-    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
-    fun getAllReposByUserName() {
-        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
-    }
-
-    // Metodo responsavel por realizar a configuracao do adapter
-    fun setupAdapter(list: List<Repository>) {
-        /*
-        @TODO 7 - Implementar a configuracao do Adapter , construir o adapter e instancia-lo
-        passando a listagem dos repositorios
-     */
-    }
-
-
-    // Metodo responsavel por compartilhar o link do repositorio selecionado
-    // @Todo 11 - Colocar esse metodo no click do share item do adapter
-    fun shareRepositoryLink(urlRepository: String) {
+    private fun shareRepositoryLink(urlRepository: String) {
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, urlRepository)
@@ -103,10 +136,7 @@ class MainActivity : AppCompatActivity() {
         startActivity(shareIntent)
     }
 
-    // Metodo responsavel por abrir o browser com o link informado do repositorio
-
-    // @Todo 12 - Colocar esse metodo no click item do adapter
-    fun openBrowser(urlRepository: String) {
+    private fun openBrowser(urlRepository: String) {
         startActivity(
             Intent(
                 Intent.ACTION_VIEW,
